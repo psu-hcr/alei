9deae8f3452f17284208989a58306d672d11bf01
zxl5344 <zxl5344@gmail.com>
2022-03-13 16:13:54 -0400

    add original pdf in data2pdf plotter

--- data2pdftestplotter.py
+++ data2pdftestplotter.py
@@ -14,8 +14,29 @@ wy = data[:, 5]
 wz = data[:, 6]
 w = data[:, 7]
 
+data1=genfromtxt('/home/zxl5344/test/src/alei/Sweeping data/ArucoPositionSample1.csv',delimiter=",",dtype=float)
+data1 = np.delete(data1,0,0)
+x1 = data1[:, 1]
+y1 = data1[:, 2]
+z1 = data1[:, 3]
+
+data2=genfromtxt('/home/zxl5344/test/src/alei/Sweeping data/ArucoPositionSample2.csv',delimiter=",",dtype=float)
+data2 = np.delete(data2,0,0)
+x2 = data2[:, 1]
+y2 = data2[:, 2]
+z2 = data2[:, 3]
+
+data3=genfromtxt('/home/zxl5344/test/src/alei/Sweeping data/ArucoPositionSample3.csv',delimiter=",",dtype=float)
+data3 = np.delete(data3,0,0)
+x3 = data3[:, 1]
+y3 = data3[:, 2]
+z3 = data3[:, 3]
+
 fig = plt.figure()
 ax = plt.axes(projection='3d')
 ax.plot3D(x,y,z, 'r')
+ax.plot3D(x1,y1,z1, 'b')
+ax.plot3D(x2,y2,z2, 'b')
+ax.plot3D(x3,y3,z3, 'b')
 plt.show()
 
