af495c9dc5145c62222261370b2bce46bd97c4cf
zxl5344 <zxl5344@gmail.com>
2022-03-14 20:59:11 -0400

    change origin of pdf

--- data2pdftest.cpp
+++ data2pdftest.cpp
@@ -36,14 +36,15 @@ int main(){
 	double dL3 = 0.05;
 	*/
 	
-	double L1 = 0.5;
-	double L2 = 0.5;
-	double L3 = 0.5;
-	double dL1 = 0.01;
-	double dL2 = 0.01;
-	double dL3 = 0.01;
+	double L1 = 0.1;
+	double L2 = 0.1;
+	double L3 = 0.1;
+	double dL1 = 0.005;
+	double dL2 = 0.005;
+	double dL3 = 0.005;
+	arma::vec new_origin = {0, 0, 0};
 	
-	data2pdf phid(Data1, Data2, Data3, L1, L2, L3, dL1, dL2, dL3);
+	data2pdf phid(Data1, Data2, Data3, L1, L2, L3, dL1, dL2, dL3, new_origin);
 	phid.calcpdf();
 	//cout<<phid.phi(90, 113, 225)<<endl;
 	cout<<"sum of phi: "<<arma::accu(phid.phi)<<endl;
@@ -55,10 +56,10 @@ int main(){
     syst1.Ucurr = unom(0); 
     random_device rd; mt19937 eng(rd());
     uniform_real_distribution<> distr(-0.4,0.4);
-    //syst1.Xcurr = {-0.2,0.0,0.1,0.0};
-    syst1.Xcurr = {distr(eng),distr(eng),distr(eng),distr(eng),distr(eng),distr(eng)};//must be initialized before instantiating cost
+    syst1.Xcurr = {-0.3,0.0,0.2,0.0,-0.2,0.0};
+    //syst1.Xcurr = {distr(eng),distr(eng),distr(eng),distr(eng),distr(eng),distr(eng)};//must be initialized before instantiating cost
     arma::mat R = 0.001*arma::eye(3,3); double q=1.;
-    arma::vec umax = {5.0, 5.0, 5.0};
+    arma::vec umax =  {5.0, 5.0, 5.0};
     double T = 0.6;
     arma::mat SIGMA = 0.01*arma::eye(3,3);	
     dklcost_pdf<dot_model, data2pdf> cost (q,R,100,SIGMA,0,2,4, &phid, L1,L2,L3,T,&syst1);	
@@ -70,12 +71,14 @@ int main(){
 	arma::vec xwrap;
 	myfile<<"time,x,y,z,wx,wy,wz, w,\n";
 	
-	while (syst1.tcurr<30.){
+	while (syst1.tcurr<100.){
 		//double start_time = omp_get_wtime();
 		cost.xmemory(syst1.Xcurr);	//cout<<"cost.xmemory"<<endl;
 		//cout <<"resamp time: "<< 1000 * (omp_get_wtime() - start_time)<<endl;
 		if(fmod(syst1.tcurr,2)<syst1.dt){
-			cout<<"Time: "<<syst1.tcurr<<"\n"<<syst1.Xcurr<<"\n";
+			cout<<"Time: "<<syst1.tcurr<<"\n";
+			cout<<"Xcurr: \n"<<syst1.Xcurr<<"\n";
+			cout<<"Ucurr: \n"<<syst1.Ucurr<<"\n";
 		}
 		xwrap = syst1.proj_func(syst1.Xcurr); 
 		myfile<<syst1.tcurr<<",";
--- src/data2pdf.hpp
+++ src/data2pdf.hpp
@@ -9,13 +9,15 @@ class data2pdf {
 	public:
 	arma::mat data1, data2, data3;
 	arma::cube phi;
+	arma::vec new_origin;
 	int n_rows, n_cols, n_slices;
 	double L1, L2, L3;
 	double dL1, dL2, dL3;
-	data2pdf (arma::mat _data1, arma::mat _data2, arma::mat _data3, double _L1, double _L2, double _L3, double _dL1, double _dL2, double _dL3){
-		// this is a class convert data from 3 datasheets into one PDF. 
+	data2pdf (arma::mat _data1, arma::mat _data2, arma::mat _data3, double _L1, double _L2, double _L3, double _dL1, double _dL2, double _dL3, arma::vec _origin){
+		// this is a class convert data from 3 datasheets into one PDF wrt to _origin. 
 		
 		data1 = _data1; data2 = _data2; data3 = _data3;	
+		new_origin = _origin;
 		L1 = _L1; L2 = _L2; L3 = _L3; 	// Boundary for PDF
 		dL1 = _dL1;  dL2 = _dL2; dL3 = _dL3;	// size of grid
 		n_rows = 2*int(L1/dL1);	cout<<"n_rows"<<n_rows<<endl;
@@ -31,9 +33,9 @@ class data2pdf {
 		//cout<<"data1"<<endl;
 		for (int i = 0; i < data1.n_rows; i++){
 			// shift x y z off center
-			double x = data1(i, 1) + L1;
-			double y = data1(i, 2) + L2;
-			double z = data1(i, 3) + L3;
+			double x = data1(i, 1) + L1 - new_origin(0);
+			double y = data1(i, 2) + L2 - new_origin(1);
+			double z = data1(i, 3) + L3 - new_origin(2);
 			
 			int n_x = int(x/dL1);	cout<<"n_x"<<n_x<<endl;
 			int n_y = int(y/dL2);	cout<<"n_y"<<n_y<<endl;
@@ -45,9 +47,9 @@ class data2pdf {
 		//cout<<"data2"<<endl;
 		for (int i = 0; i < data2.n_rows; i++){
 			// shift x y z off center
-			double x = data2(i, 1) + L1;
-			double y = data2(i, 2) + L2;
-			double z = data2(i, 3) + L3;
+			double x = data2(i, 1) + L1 - new_origin(0);
+			double y = data2(i, 2) + L2 - new_origin(1);
+			double z = data2(i, 3) + L3 - new_origin(2);
 			
 			int n_x = int(x/dL1);	cout<<"n_x"<<n_x<<endl;
 			int n_y = int(y/dL2);	cout<<"n_y"<<n_y<<endl;
@@ -59,9 +61,9 @@ class data2pdf {
 		//cout<<"data3"<<endl;
 		for (int i = 0; i < data3.n_rows; i++){
 			// shift x y z off center
-			double x = data3(i, 1) + L1;
-			double y = data3(i, 2) + L2;
-			double z = data3(i, 3) + L3;
+			double x = data3(i, 1) + L1 - new_origin(0);
+			double y = data3(i, 2) + L2 - new_origin(1);
+			double z = data3(i, 3) + L3 - new_origin(2);
 			
 			int n_x = int(x/dL1);	cout<<"n_x"<<n_x<<endl;
 			int n_y = int(y/dL2);	cout<<"n_y"<<n_y<<endl;
