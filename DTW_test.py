9584be8b64a2fe3acfaa97ea141b40027ba98108
zxl5344 <zxl5344@gmail.com>
2022-03-18 16:45:24 -0400

    test DTW distance between different segment

--- DTW_test.py
+++ DTW_test.py
@@ -3,27 +3,41 @@ import numpy as np
 from math import pi
 import time
 from dtw import dtw
+from scipy.spatial.distance import cityblock
+
+norm_distance = lambda x, y: cityblock(x,y)
 
 def main():
 
 	data1 = np.genfromtxt('/home/zxl5344/test/src/alei/Gaussian_traj/CameraRecording1.csv',delimiter=",",dtype=float)
-	x1 = data1[:, 1]
-	y1 = data1[:, 2]
-	z1 = data1[:, 3]
 
 	data2 = np.genfromtxt('/home/zxl5344/test/src/alei/Gaussian_traj/CameraRecording2.csv',delimiter=",",dtype=float)
-	x2 = data2[:, 1]
-	y2 = data2[:, 2]
-	z2 = data2[:, 3]
 
 	data3 = np.genfromtxt('/home/zxl5344/test/src/alei/Gaussian_traj/CameraRecording3.csv',delimiter=",",dtype=float)
-	x3 = data3[:, 1]
-	y3 = data3[:, 2]
-	z3 = data3[:, 3]
 	
-	seg1 = np.genfromtxt('/home/zxl5344/test/src/alei/Gaussian_traj/CameraRecording1_seg.csv',delimiter=",",dtype=float)
-	seg2 = np.genfromtxt('/home/zxl5344/test/src/alei/Gaussian_traj/CameraRecording2_seg.csv',delimiter=",",dtype=float)
-	seg3 = np.genfromtxt('/home/zxl5344/test/src/alei/Gaussian_traj/CameraRecording3_seg.csv',delimiter=",",dtype=float)
+	seg1 = np.genfromtxt('/home/zxl5344/test/src/alei/Gaussian_traj/CameraRecording1_seg.csv',delimiter=",",dtype=int)
+	seg2 = np.genfromtxt('/home/zxl5344/test/src/alei/Gaussian_traj/CameraRecording2_seg.csv',delimiter=",",dtype=int)
+	seg3 = np.genfromtxt('/home/zxl5344/test/src/alei/Gaussian_traj/CameraRecording3_seg.csv',delimiter=",",dtype=int)
+	
+	x11 = data1[0:seg1[0], 1:4]
+	x12 = data1[seg1[1]:seg1[2], 1:4]
+	x13 = data1[seg1[2]:seg1[3], 1:4]
+	x14 = data1[seg1[3]:seg1[4], 1:4]
+	x21 = data2[0:seg2[0], 1:4]
+	x22 = data2[seg2[1]:seg2[2], 1:4]
+	x23 = data2[seg2[2]:seg2[3], 1:4]
+	x31 = data3[0:seg3[0], 1:4]
+	x32 = data3[seg3[1]:seg3[2], 1:4]
+	x33 = data3[seg3[2]:seg3[3], 1:4]
+	
+	d11_12 = dtw(x11, x12, dist=norm_distance)
+	d11_13 = dtw(x11, x13, dist=norm_distance)
+	d11_14 = dtw(x11, x14, dist=norm_distance)
+	d11_21 = dtw(x11, x21, dist=norm_distance)
+	d11_31 = dtw(x11, x31, dist=norm_distance)
+	d11_32 = dtw(x11, x32, dist=norm_distance)
+	
+	print(d11_12, d11_13, d11_14, d11_21, d11_31, d11_32)
 
 
 if __name__=='__main__':
